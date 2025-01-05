f5644e8147365c162ef24e5a98218da944f1ed16
NealHangLu <neallvhang@126.com>
2025-01-05 11:49:23 +0800

    update js elem

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'Projects', 'Workings']
+const section_names = ['home', 'projects', 'experiences']
 
 
 window.addEventListener('DOMContentLoaded', event => {
